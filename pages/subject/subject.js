1f860ec73a1ca68fe75c864604394e61a73583f7
caochangkui <1243960844@qq.com>
2018-09-15 11:03:11 +0800

    [update] 公共组件分离 common template

--- pages/subject/subject.js
+++ pages/subject/subject.js
@@ -5,7 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    list: [
+      {
+        type: "动作",
+        url: "/assets/image/dongzuo.png"
+      },
+      {
+        type: "科幻",
+        url: "/assets/image/kehuan.png"
+      },
+      {
+        type: "动画",
+        url: "/assets/image/donghua.png"
+      },
+      {
+        type: "爱情",
+        url: "/assets/image/aiqing.png"
+      },
+      {
+        type: "喜剧",
+        url: "/assets/image/xiju.png"
+      }
+    ]
   },
 
   /**
