81d7f8eaee8bc9e75aed337eb4a6d14e52382594
caochangkui <1243960844@qq.com>
2018-09-15 13:10:21 +0800

    [add] gerUserInfo

--- pages/user/user.js
+++ pages/user/user.js
@@ -5,14 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    userInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.getUserInfo({
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo
+        });
+      }
+    })
+  },
+
+  getUser (e) {
+    console.log(e)
+  },
+
+  // 存储信息到storage
+  set () {
+    wx.setStorage({
+      key: 'user',
+      data: 'cck',
+    })
+  },
+
+  // 从storage 中提取信息
+  get () {
+    wx.getStorage({
+      key: 'user',
+      success: function(res) {
+        console.log(res);
+      },
+    })
   },
 
   /**
