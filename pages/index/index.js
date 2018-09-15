ff918cc6788d14cf846e3899ad3e727aa3297564
caochangkui <1243960844@qq.com>
2018-09-15 15:13:28 +0800

    [change]

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,56 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list: [],
-    start: 0,
-    loading: false // 是否正在加载
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.loadData();
-  },
-
-  // 上拉加载
-  lower () {
-    console.log('lower');
-    if (!this.data.loading) {
-      this.loadData();
-    } 
-  },
-
-  tap (e) {
-    console.log(e.currentTarget.dataset.id);
-    wx.navigateTo({
-      url: `/pages/detail/detail?id=${e.currentTarget.dataset.id}`,
-    })
-  },
-
-  loadData () {
-    let { start, list } = this.data;
-    wx.showLoading({
-      title: '正在加载...',
-      mask: true
-    });
-    this.setData({
-      loading: true
-    });
-    wx.request({
-      url: `https://data.miaov.com/h5-view/v/movie/list/?start=${this.data.start}`,
-      success: (res) => { 
-        console.log(res.data.subjects);
-        start += 20;
-        list.push( ...res.data.subjects );
-        this.setData({
-          list: list,
-          start: start,
-          loading: false
-        });
-        wx.hideLoading();
-      }
-    })
+  
   },
 
   /**
