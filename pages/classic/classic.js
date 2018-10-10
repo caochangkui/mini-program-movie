c820c6a4b9e651460ddaefc1ac6a062c78ce8aa8
caochangkui <1243960844@qq.com>
2018-10-10 17:18:27 +0800

    [init]

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,46 +1,18 @@
-// pages/subject/subject.js
+// pages/classic/classic.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list: [
-      {
-        type: "动作",
-        url: "/assets/image/dongzuo.png"
-      },
-      {
-        type: "科幻",
-        url: "/assets/image/kehuan.png"
-      },
-      {
-        type: "动画",
-        url: "/assets/image/donghua.png"
-      },
-      {
-        type: "爱情",
-        url: "/assets/image/aiqing.png"
-      },
-      {
-        type: "喜剧",
-        url: "/assets/image/xiju.png"
-      }
-    ]
-  },
-
-  tap (e) {
-    console.log(e.currentTarget.dataset.type);
-    wx.navigateTo({
-      url: `/pages/subject-list/subject-list?type=${e.currentTarget.dataset.type}`,
-    })
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+  
   },
 
   /**
